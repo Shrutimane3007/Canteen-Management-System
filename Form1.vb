65a9ff30fee2851d7b0e3541029f12abd0ad7d62
Shrutimane <125731137+Shrutimane3007@users.noreply.github.com>
2023-05-07 15:36:56 +0530

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,113 @@
+﻿Public Class Form1
+
+    Dim total As Double = 0 'Total bill amount
+    Dim grandTotal As Double = 0 'Grand total bill amount
+    Dim ItemPrice As Double 'Price of the selected item
+    Dim itemName As String 'Name of the selected item
+    Dim quantity As Integer 'Quantity of the selected item
+    Dim itemTotal As Double 'Total cost of the selected item
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Initialize the list of items
+        cboItems.Items.Add("Burger(Rs150.00)")
+        cboItems.Items.Add("Pizza(Rs300.00)")
+        cboItems.Items.Add("Fries(Rs70.00)")
+        cboItems.Items.Add("Coke(Rs30.00)")
+        cboItems.Items.Add("Tea(Rs12.00)")
+        cboItems.Items.Add("Coffee(Rs20.00)")
+        cboItems.Items.Add("SandWhich(Rs35.00)")
+        cboItems.Items.Add("Vadapav(Rs14.00)")
+        cboItems.Items.Add("Fried Rice(Rs45.00)")
+        cboItems.Items.Add("Momos(Rs25.00)")
+        cboItems.SelectedIndex = 0 'Set the default item to the first one in the list
+
+        'Set the currency symbol to Indian rupee
+        txtTotal.Text = "₹0.00"
+        txtGrandTotal.Text = "₹0.00"
+    End Sub
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        'Calculate the total cost of the selected item
+        ItemPrice = getItemPrice(cboItems.SelectedIndex)
+        quantity = CInt(txtQuantity.Text)
+        itemTotal = itemPrice * quantity
+
+        ' Validate quantity
+        If Not Integer.TryParse(txtQuantity.Text, quantity) Or quantity <= 0 Then
+            MessageBox.Show("Please enter a valid positive quantity.", "Canteen Management System", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        'Add the item to the list box and update the total bill amount
+        lstItems.Items.Add(txtQuantity.Text & " x " & cboItems.SelectedItem & " - ₹" & itemTotal.ToString("0.00"))
+        total += itemTotal
+        txtTotal.Text = "₹" & total.ToString("0.00")
+
+        'Clear the quantity textbox
+        txtQuantity.Text = ""
+    End Sub
+
+    Private Function getItemPrice(index As Integer) As Double
+        'Return the price of the selected item in INR
+        Select Case index
+            Case 0 'Burger
+                Return 150.0
+            Case 1 'Pizza
+                Return 300.0
+            Case 2 'Fries
+                Return 70.0
+            Case 3 'Coke
+                Return 30.0
+            Case 4 'Tea
+                Return 12.0
+            Case 5 'Coffee
+                Return 20.0
+            Case 6 'SandWhich
+                Return 35.0
+            Case 7 'Vadapav
+                Return 14.0
+            Case 8 'Fried Rice
+                Return 45.0
+            Case 9 'Momos
+                Return 25.0
+            Case Else 'Invalid index
+                Return 0
+        End Select
+    End Function
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear the list box and reset the total bill amount
+        lstItems.Items.Clear()
+        total = 0
+        txtTotal.Text = "₹0.00"
+        txtGrandTotal.Text = "₹0.00"
+    End Sub
+
+    Private Sub btnPrintBill_Click(sender As Object, e As EventArgs) Handles btnPrintBill.Click
+        ' Check if cart is empty
+        If lstItems.Items.Count = 0 Then
+            MessageBox.Show("Please add some items to the cart before generating a bill.", "Canteen Management System", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        ' Display bill
+        Dim bill As String = ""
+        For Each item As Object In lstItems.Items
+            bill += item.ToString() & vbCrLf
+        Next
+        bill += "----------------------------------------------" & vbCrLf
+        bill += "Total: " & total.ToString("C") & vbCrLf
+        bill += "VAT: " & (total * 0.14).ToString("C") & vbCrLf
+        bill += "Grand Total: " & (total * 1.14).ToString("C")
+        MessageBox.Show(bill, "Canteen Management System - Bill", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        ' Clear cart
+        lstItems.Items.Clear()
+        total = 0
+        txtTotal.Text = "₹0.00"
+
+    End Sub
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close the form
+        Me.Close()
+    End Sub
+
+End Class
---
+++ canteen management system GAD.Designer.vb
@@ -0,0 +1,226 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.cboItems = New System.Windows.Forms.ComboBox()
+        Me.txtQuantity = New System.Windows.Forms.TextBox()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.txtGrandTotal = New System.Windows.Forms.TextBox()
+        Me.lstItems = New System.Windows.Forms.ListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnPrintBill = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(495, 78)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(736, 452)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Honeydew
+        Me.Label1.Location = New System.Drawing.Point(430, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(185, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "CANTEEN MANAGEMENT SYSTEM"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Honeydew
+        Me.Label2.Location = New System.Drawing.Point(12, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "ITEMS"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Honeydew
+        Me.Label3.Location = New System.Drawing.Point(13, 117)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(102, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "ENTER QUANTITY"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Honeydew
+        Me.Label4.Location = New System.Drawing.Point(13, 168)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(42, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "TOTAL"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Honeydew
+        Me.Label5.Location = New System.Drawing.Point(12, 206)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(84, 13)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "GRAND TOTAL"
+        '
+        'cboItems
+        '
+        Me.cboItems.DisplayMember = "ItemPrice"
+        Me.cboItems.FormattingEnabled = True
+        Me.cboItems.Location = New System.Drawing.Point(12, 78)
+        Me.cboItems.Name = "cboItems"
+        Me.cboItems.Size = New System.Drawing.Size(197, 21)
+        Me.cboItems.TabIndex = 8
+        Me.cboItems.ValueMember = "ItemPrice"
+        '
+        'txtQuantity
+        '
+        Me.txtQuantity.Location = New System.Drawing.Point(12, 133)
+        Me.txtQuantity.Name = "txtQuantity"
+        Me.txtQuantity.Size = New System.Drawing.Size(100, 20)
+        Me.txtQuantity.TabIndex = 9
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(109, 168)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(100, 20)
+        Me.txtTotal.TabIndex = 10
+        '
+        'txtGrandTotal
+        '
+        Me.txtGrandTotal.Location = New System.Drawing.Point(109, 206)
+        Me.txtGrandTotal.Name = "txtGrandTotal"
+        Me.txtGrandTotal.Size = New System.Drawing.Size(100, 20)
+        Me.txtGrandTotal.TabIndex = 11
+        '
+        'lstItems
+        '
+        Me.lstItems.FormattingEnabled = True
+        Me.lstItems.Location = New System.Drawing.Point(12, 242)
+        Me.lstItems.Name = "lstItems"
+        Me.lstItems.Size = New System.Drawing.Size(436, 212)
+        Me.lstItems.TabIndex = 12
+        '
+        'btnAdd
+        '
+        Me.btnAdd.BackColor = System.Drawing.Color.Honeydew
+        Me.btnAdd.Location = New System.Drawing.Point(12, 491)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 13
+        Me.btnAdd.Text = "ADD"
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.Honeydew
+        Me.btnClear.Location = New System.Drawing.Point(134, 491)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 14
+        Me.btnClear.Text = "CLEAR"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'btnPrintBill
+        '
+        Me.btnPrintBill.BackColor = System.Drawing.Color.Honeydew
+        Me.btnPrintBill.Location = New System.Drawing.Point(256, 491)
+        Me.btnPrintBill.Name = "btnPrintBill"
+        Me.btnPrintBill.Size = New System.Drawing.Size(75, 23)
+        Me.btnPrintBill.TabIndex = 15
+        Me.btnPrintBill.Text = "PRINT BILL"
+        Me.btnPrintBill.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Honeydew
+        Me.btnExit.Location = New System.Drawing.Point(373, 491)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 16
+        Me.btnExit.Text = "EXIT"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkSlateGray
+        Me.ClientSize = New System.Drawing.Size(1243, 542)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnPrintBill)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.lstItems)
+        Me.Controls.Add(Me.txtGrandTotal)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.txtQuantity)
+        Me.Controls.Add(Me.cboItems)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents cboItems As ComboBox
+    Friend WithEvents txtQuantity As TextBox
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents txtGrandTotal As TextBox
+    Friend WithEvents lstItems As ListBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnPrintBill As Button
+    Friend WithEvents btnExit As Button
+End Class
